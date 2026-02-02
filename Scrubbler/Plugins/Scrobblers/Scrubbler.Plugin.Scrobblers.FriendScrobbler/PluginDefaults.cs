9733a38be0c70f20806256609515c240022bc7e1
Tim Stadler <timstadler905@googlemail.com>
2026-02-02 20:22:09 +0100

    Put FriendScrobbler in its own repo

--- Scrubbler/Plugins/Scrobblers/Scrubbler.Plugin.Scrobblers.FriendScrobbler/PluginDefaults.cs
+++
@@ -1,7 +0,0 @@
-namespace Scrubbler.Plugin.Scrobblers.FriendScrobbler;
-
-internal static class PluginDefaults
-{
-    public const string ApiKey = "LASTFM_API_KEY";
-    public const string ApiSecret = "LASTFM_API_SECRET";
-}
