6ddb33a84d903cdc1e678c4195cc09e6f5118afd
Tim Stadler <timstadler905@googlemail.com>
2026-02-12 12:39:38 +0100

    Add dependencies submodule

--- Scrubbler/Plugins/Scrobblers/Scrubbler.Plugin.Scrobbler.Mock/PluginSettings.cs
+++
@@ -1,8 +0,0 @@
-using Scrubbler.Abstractions.Settings;
-
-namespace Scrubbler.Plugin.Scrobbler.Mock;
-
-internal class PluginSettings : IPluginSettings
-{
-    public string SomeSetting { get; set; } = "TestSetting";
-}
